33cb44bf3d38be85d0fff1d085b99fceaf5a3797
obada <obadaquraan@yahoo.com>
2019-11-18 14:05:46 +0200

    fix

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-server.use( cors() );
+// server.use( cors() );
 
 app.get('/location', (request, response) => {
     // send the users current location back to them
