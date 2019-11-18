cb4f733612897b2d990001e4e0f51d17a95f299f
obada <obadaquraan@yahoo.com>
2019-11-18 11:42:45 +0200

    initial files

---
+++ server.js
@@ -0,0 +1,33 @@
+'use strict'
+
+// npm packages - 3rd party
+
+// npm packages - 3rd party
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+
+// application constant
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/location', (request, response) => {
+    // send the users current location back to them
+    const geoData = require('./data/geo.json');
+    const city = request.query.data;
+    const locationData = new Location(city, geoData);
+    
+    response.send(locationData);
+  });
+  
+  function Location(city, geoData) {
+    this.search_query = city;
+    this.formatted_query = geoData.results[0].formatted_address;
+    this.latitude = geoData.results[0].geometry.location.lat;
+    this.longitude = geoData.results[0].geometry.location.lng;
+  }
+  
+  app.listen(PORT, () => {
+    console.log(`listening on PORT ${PORT}`);
+  });
